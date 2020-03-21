97f2de5ebc086eac16e394435948d0e92981da1f
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-21 12:18:09 -0500

    Including tests for Proptypes in Input component and code to pass them

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
+const defaultProps = { secretWord: 'party' };
+
 const setup = () => {
-  return shallow(<Input />);
+  return shallow(<Input {...defaultProps} />);
 }
 
 test('Input component render without errors', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('Does not warning with expected props', () => {
+  const expectedProps = { secretWord: 'party' };
+  checkProps(Input, expectedProps);
 });
