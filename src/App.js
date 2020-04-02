182c2da5e8417861fae321f849922a515a9e0644
Carlos Gonzalez <carlos@sancrisoft.com>
2020-04-01 19:05:40 -0500

    Including hook reducer in App and test to validate method is executed in useEffect initial and not in every update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
 
-import Congrats from './Congrats';
-import GuessedWords from './GuessedWords';
 import './App.css';
+import hookActions from './actions/hookActions';
+
+/**
+ * Reducer to update state, called automatically by dispatch
+ * @param {object} state - existing state
+ * @param {object} action - contains `type` and `payload` properties for the state update (like using redux)
+ * @returns {object} - new state
+ */
+function reducer(state, action) {
+  switch(action.type) {
+    case 'setSecretWord':
+      return {...state, secretWord: action.payload}
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
 
 function App() {
-  const guessedWordsTest = [
-    { guessedWord: "train", letterMatchCount: 3 },
-    { guessedWord: "agile", letterMatchCount: 1 }
-  ];
+  const [state, dispatch] = React.useReducer(reducer, {
+    secretWord: null,
+  });
+
+  const setSecretWord = (secretWord) => dispatch({
+    type: 'setSecretWord',
+    payload: secretWord,
+  });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord)
+  }, []);
+
   return (
-    <div
-      data-test="component-app"
-      className="container"
-    >
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords guessedWords={guessedWordsTest} />
+    <div data-test="component-app">
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { mount, ShallowWrapper } from 'enzyme';
 
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup funtion for app component
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // Use mount, because useEffect is not called on `shallow`
+  return mount(<App />);
 }
 
 test('App renders without errors', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord calls on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+  test('getSecretWord does not update on App update', () => {
+    const wrapper = setup();
+    // Clear mock method because is executed on mount
+    mockGetSecretWord.mockClear();
+
+    // Wrapper.update() doesn't trigger update
+    // issue in github (from https://github.com/airbnb/enzyme/issues/2091)
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
 });
